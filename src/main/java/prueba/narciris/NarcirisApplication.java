6a18b9cb0a98b5d35d218ecd8b1d5b62e43b006e
SoyNar <narciris.tech@gmail.com>
2026-08-11 09:12:10 -0500

    feat: subiendo proyecto

---
+++ src/main/java/prueba/narciris/NarcirisApplication.java
@@ -0,0 +1,13 @@
+package prueba.narciris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarcirisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarcirisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prueba/narciris/NarcirisApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.narciris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarcirisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
